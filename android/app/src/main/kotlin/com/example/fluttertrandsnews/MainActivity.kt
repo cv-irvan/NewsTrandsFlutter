8c6785efca99432126856a9d1117e36addb30d42
irvan <irvanpartii@gmail.com>
2020-12-24 08:22:41 +0700

    flutter work space

---
+++ android/app/src/main/kotlin/com/example/fluttertrandsnews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttertrandsnews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeNews extends StatefulWidget {
+  @override
+  _HomeNewsState createState() => _HomeNewsState();
+}
+
+class _HomeNewsState extends State<HomeNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
